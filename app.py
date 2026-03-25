c0333684a25d8249b7d7af784fb674860393599a
qiujiachen45-png <qiujiachen45@gmail.com>
2026-03-25 22:32:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ if query:
 
 # 页脚
 st.markdown("---")
-st.caption("© 2024 企业 RAG 智能问答系统 - 基于 Streamlit + FAISS + DeepSeek")
+st.caption("© 2026 workshop企业RAG 智能问答系统 - 基于 Streamlit + FAISS + DeepSeek" QiujiachenACCT大一)
